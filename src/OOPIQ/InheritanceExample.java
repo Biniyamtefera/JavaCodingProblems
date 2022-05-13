4f9edc2ad366a9cb748418dc72f72ab81d382875
Biniyamtefera <75863208+Biniyamtefera@users.noreply.github.com>
2022-05-12 20:47:19 -0600

    OOP concept also add for Interview preparation

---
+++ src/OOPIQ/InheritanceExample.java
@@ -0,0 +1,14 @@
+package OOPIQ;
+
+public class InheritanceExample {
+    int add(int i,int j) {
+        return i + j;
+    }
+
+    public class B extends InheritanceExample {
+       // public static void main(String[] args) {
+
+        }
+    }
+
+
